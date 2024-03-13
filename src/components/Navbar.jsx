57f18d4578dfbea8f00270e15ab1ba23d956f238
Panha <76092165+Panha13@users.noreply.github.com>
2024-03-14 04:16:25 +0700

    navbar1.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,61 @@
 import { BiSearch, BiHeart, BiCartAlt, BiUser } from "react-icons/bi";
 import { AiOutlineMenu } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 768 && setMenu(false)
+    );
+  }, []);
+
   return (
     <>
-      <nav className="px-10 lg:px-32 flex items-center justify-between max-container h-14">
-        <a href="#" className="font-semibold text-4xl">
-          Fabraze
-        </a>
-        <ul className="flex uppercase text-md gap-5 font-medium max-md:hidden">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Women</a>
-          </li>
-          <li>
-            <a href="#">Men</a>
-          </li>
-          <li>
-            <a href="#">Boys</a>
-          </li>
-          <li>
-            <a href="#">Girls</a>
-          </li>
-        </ul>
-        <div className="flex gap-3 text-2xl">
-          <a href="#">
-            <BiSearch />
+      <nav className="px-5 lg:px-32 bg-white max-container">
+        <div className="flex items-center justify-between h-14">
+          <a href="#" className="font-semibold text-4xl">
+            Fabraze
           </a>
-          <a href="#">
-            <BiHeart />
-          </a>
-          <a href="#" className="max-md:hidden">
-            <BiCartAlt />
-          </a>
-          <a href="#">
-            <BiUser />
-          </a>
-          <button className="md:hidden">
-            <AiOutlineMenu />
-          </button>
+          <ul className="flex uppercase text-md gap-5 font-medium max-md:hidden">
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-3 text-2xl">
+            <a href="#">
+              <BiSearch />
+            </a>
+            <a href="#">
+              <BiHeart />
+            </a>
+            <a href="#" className="max-md:hidden">
+              <BiCartAlt />
+            </a>
+            <a href="#">
+              <BiUser />
+            </a>
+            <button onClick={toggleMenu} className="md:hidden">
+              <AiOutlineMenu />
+            </button>
+          </div>
+        </div>
+        <div className={`pb-3  ${menu ? "" : "hidden"}`}>
+          <ul className="block uppercase text-md font-medium">
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
         </div>
       </nav>
     </>
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { href: "#home", label: "Home" },
+  { href: "#women", label: "Women" },
+  { href: "#men", label: "Men" },
+  { href: "#boys", label: "Boys" },
+  { href: "#girls", label: "Girls" },
+];
