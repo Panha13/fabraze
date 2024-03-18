60b40aae54ecfd0378453b554b8d0d1fd0c628e6
Panha <76092165+Panha13@users.noreply.github.com>
2024-03-18 23:02:55 +0700

    fail

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -43,57 +43,55 @@ const Slideshow = () => {
     };
   }, [curr]);
   return (
-    <Swipeable onSwipedLeft={handleSwipeLeft} onSwipedRight={handleSwipeRight}>
-      <section className="flex items-center overflow-hidden relative">
-        <div
-          className={`flex transition-transform ease-out duration-500 translate-x-[-${
-            curr * 100
-          }vw]`}
-        >
-          {slideData.map((item) => (
-            <div key={item.id} className={`w-screen bg-[${item.bg}]`}>
-              <div className="flex flex-col md:flex-row justify-between items-center max-container px-5 lg:px-20 h-svh">
-                <div className="inline-block text-center md:text-left w-full mt-12 mb-5">
-                  <h3 className="text-md md:text-xl font-medium tracking-widest uppercase">
-                    <span># </span>
-                    {item.subtitle}
-                  </h3>
-                  <h1 className="text-5xl md:text-7xl font-bold uppercase my-4">
-                    {item.title}
-                  </h1>
-                  <div className="flex  max-md:justify-center">
-                    <hr className="w-32 rounded border-2 border-red" />
-                  </div>
-                  <p className="text-sm text-dark my-4 md:w-2/3">{item.desc}</p>
-                  <button className="uppercase py-2 px-4 mt-5 text-md bg-darkest text-white">
-                    Shop Now
-                  </button>
+    <section className="flex items-center overflow-hidden relative">
+      <div
+        className={`flex transition-transform ease-out duration-500 translate-x-[-${
+          curr * 100
+        }vw]`}
+      >
+        {slideData.map((item) => (
+          <div key={item.id} className={`w-screen bg-[${item.bg}]`}>
+            <div className="flex flex-col md:flex-row justify-between items-center max-container px-5 lg:px-20 h-svh">
+              <div className="inline-block text-center md:text-left w-full mt-12 mb-5">
+                <h3 className="text-md md:text-xl font-medium tracking-widest uppercase">
+                  <span># </span>
+                  {item.subtitle}
+                </h3>
+                <h1 className="text-5xl md:text-7xl font-bold uppercase my-4">
+                  {item.title}
+                </h1>
+                <div className="flex  max-md:justify-center">
+                  <hr className="w-32 rounded border-2 border-red" />
                 </div>
-                <img
-                  src={item.image}
-                  className="object-cover h-full"
-                  alt="Slideshow"
-                />
+                <p className="text-sm text-dark my-4 md:w-2/3">{item.desc}</p>
+                <button className="uppercase py-2 px-4 mt-5 text-md bg-darkest text-white">
+                  Shop Now
+                </button>
               </div>
+              <img
+                src={item.image}
+                className="object-cover h-full"
+                alt="Slideshow"
+              />
             </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute bottom-8 right-0 left-0">
+        <div className="flex justify-center items-center gap-1">
+          {slideData.map((_, index) => (
+            <div
+              key={index}
+              className={`w-3 h-3  bg-${
+                index === curr ? "dark" : "dark/30"
+              } rounded-full cursor-pointer transition-colors duration-500`}
+            ></div>
           ))}
         </div>
-        <div className="absolute bottom-8 right-0 left-0">
-          <div className="flex justify-center items-center gap-1">
-            {slideData.map((_, index) => (
-              <div
-                key={index}
-                className={`w-3 h-3  bg-${
-                  index === curr ? "dark" : "dark/30"
-                } rounded-full cursor-pointer transition-colors duration-500`}
-              ></div>
-            ))}
-          </div>
-        </div>
-        <ArrowButton direction="left" onClick={prev} Icon={GoArrowLeft} />
-        <ArrowButton direction="right" onClick={next} Icon={GoArrowRight} />
-      </section>
-    </Swipeable>
+      </div>
+      <ArrowButton direction="left" onClick={prev} Icon={GoArrowLeft} />
+      <ArrowButton direction="right" onClick={next} Icon={GoArrowRight} />
+    </section>
   );
 };
 
