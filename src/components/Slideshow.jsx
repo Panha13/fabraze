5e35a460435d9ff7129d61d3bedabeeec984be80
Panha <76092165+Panha13@users.noreply.github.com>
2024-03-18 22:26:49 +0700

    slideshow end

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,94 +1,88 @@
-import React from "react";
-import slideImg1 from "../assets/1.png";
-import slideImg2 from "../assets/2.png";
-import slideImg3 from "../assets/3.png";
+import React, { useEffect, useState } from "react";
+import { GoArrowRight, GoArrowLeft } from "react-icons/go";
+import { slideData } from "../constants";
+
+const ArrowButton = ({ direction, onClick, Icon }) => {
+  return (
+    <button
+      className={`absolute ${direction}-0 transition-all ease-in-out duration-300 hover:bg-white rounded-full p-2 mx-4 text-2xl`}
+      onClick={onClick}
+    >
+      <Icon className="text-dark/30" />
+    </button>
+  );
+};
 
 const Slideshow = () => {
+  const [curr, setCurr] = useState(0);
+  const [autoSlideInterval, setAutoSlideInterval] = useState(null);
+
+  const prev = () =>
+    setCurr((curr) => (curr === 0 ? slideData.length - 1 : curr - 1));
+  const next = () =>
+    setCurr((curr) => (curr === slideData.length - 1 ? 0 : curr + 1));
+
+  // AutoSlide
+  useEffect(() => {
+    const interval = setInterval(() => {
+      next();
+    }, 10000);
+
+    setAutoSlideInterval(interval);
+
+    return () => {
+      clearInterval(autoSlideInterval);
+    };
+  }, [curr]);
   return (
-    <section className="flex overflow-hidden">
-      <div className="flex ">
-        <div className="w-screen bg-light z-0">
-          <div className="flex flex-col md:flex-row justify-between items-center max-container px-5 lg:px-20 h-svh">
-            <div className="inline-block text-center md:text-left mt-12 mb-5">
-              <h3 className="text-xl font-medium tracking-widest uppercase">
-                <span># </span>New Arrivals
-              </h3>
-              <h1 className="text-7xl font-bold uppercase my-4">
-                Women Fusion
-              </h1>
-              <div className="flex  max-md:justify-center">
-                <hr className="w-32 rounded border-2 border-red" />
+    <section className="flex items-center overflow-hidden relative">
+      <div
+        className={`flex transition-transform ease-out duration-500 translate-x-[-${
+          curr * 100
+        }vw]`}
+      >
+        {slideData.map((item) => (
+          <div key={item.id} className={`w-screen bg-[${item.bg}]`}>
+            <div className="flex flex-col md:flex-row justify-between items-center max-container px-5 lg:px-20 h-svh">
+              <div className="inline-block text-center md:text-left w-full mt-12 mb-5">
+                <h3 className="text-md md:text-xl font-medium tracking-widest uppercase">
+                  <span># </span>
+                  {item.subtitle}
+                </h3>
+                <h1 className="text-5xl md:text-7xl font-bold uppercase my-4">
+                  {item.title}
+                </h1>
+                <div className="flex  max-md:justify-center">
+                  <hr className="w-32 rounded border-2 border-red" />
+                </div>
+                <p className="text-sm text-dark my-4 md:w-2/3">{item.desc}</p>
+                <button className="uppercase py-2 px-4 mt-5 text-md bg-darkest text-white">
+                  Shop Now
+                </button>
               </div>
-              <p className="text-sm text-dark my-4 md:w-2/3">
-                Discover a blend of fashion and comfort in our Women's Fusion
-                collection
-              </p>
-              <button className="uppercase py-2 px-4 mt-5 text-md bg-darkest text-white">
-                Shop Now
-              </button>
+              <img
+                src={item.image}
+                className="object-cover h-full"
+                alt="Slideshow"
+              />
             </div>
-            <img
-              src={slideImg1}
-              className="object-cover h-full"
-              alt="Slideshow"
-            />
           </div>
-        </div>
-        <div className="w-screen bg-[#FFE8E8] z-0">
-          <div className="flex flex-col md:flex-row justify-between items-center max-container px-5 lg:px-20 h-svh">
-            <div className="inline-block text-center md:text-left mt-12 mb-5">
-              <h3 className="text-xl font-medium tracking-widest uppercase">
-                <span># </span>New Arrivals
-              </h3>
-              <h1 className="text-7xl font-bold uppercase my-4">
-                Women Fusion
-              </h1>
-              <div className="flex  max-md:justify-center">
-                <hr className="w-32 rounded border-2 border-red" />
-              </div>
-              <p className="text-sm text-dark my-4 md:w-2/3">
-                Discover a blend of fashion and comfort in our Women's Fusion
-                collection
-              </p>
-              <button className="uppercase py-2 px-4 mt-5 text-md bg-darkest text-white">
-                Shop Now
-              </button>
-            </div>
-            <img
-              src={slideImg2}
-              className="object-cover h-full"
-              alt="Slideshow"
-            />
-          </div>
-        </div>
-        <div className="w-screen bg-[#EDE6FF] z-0">
-          <div className="flex flex-col md:flex-row justify-between items-center max-container px-5 lg:px-20 h-svh">
-            <div className="inline-block text-center md:text-left mt-12 mb-5">
-              <h3 className="text-xl font-medium tracking-widest uppercase">
-                <span># </span>New Arrivals
-              </h3>
-              <h1 className="text-7xl font-bold uppercase my-4">
-                Women Fusion
-              </h1>
-              <div className="flex  max-md:justify-center">
-                <hr className="w-32 rounded border-2 border-red" />
-              </div>
-              <p className="text-sm text-dark my-4 md:w-2/3">
-                Discover a blend of fashion and comfort in our Women's Fusion
-                collection
-              </p>
-              <button className="uppercase py-2 px-4 mt-5 text-md bg-darkest text-white">
-                Shop Now
-              </button>
-            </div>
-            <img
-              src={slideImg3}
-              className="object-cover h-full"
-              alt="Slideshow"
-            />
-          </div>
+        ))}
+      </div>
+      <div className="absolute bottom-8 right-0 left-0">
+        <div className="flex justify-center items-center gap-1">
+          {slideData.map((_, index) => (
+            <div
+              key={index}
+              className={`w-3 h-3  bg-${
+                index === curr ? "dark" : "dark/30"
+              } rounded-full cursor-pointer transition-colors duration-500`}
+            ></div>
+          ))}
         </div>
       </div>
+      <ArrowButton direction="left" onClick={prev} Icon={GoArrowLeft} />
+      <ArrowButton direction="right" onClick={next} Icon={GoArrowRight} />
     </section>
   );
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -5,3 +5,35 @@ export const navLinks = [
   { href: "#boys", label: "Boys" },
   { href: "#girls", label: "Girls" },
 ];
+
+export const slideData = [
+  {
+    id: 1,
+    title: "New Arrivals",
+    subtitle: "Discover the Latest Trends",
+    desc: "Explore our latest collection of trendy clothing and accessories.",
+    image: "https://i.ibb.co/g97907v/1.png",
+    bg: "#E6F5FF",
+  },
+  {
+    id: 2,
+    title: "Spring Collection",
+    subtitle: "Fresh Styles for the Season",
+    desc: "Step into spring with our vibrant collection of dresses, tops, and more.",
+    image: "https://i.ibb.co/k1fBR4Q/2.png",
+    bg: "#FFF5E6",
+  },
+  {
+    id: 3,
+    title: "Summer Essentials",
+    subtitle: "Get Ready for Fun in the Sun",
+    desc: "Find everything you need for a stylish summer wardrobe.",
+    image: "https://i.ibb.co/bswRv5N/3.png",
+    bg: "#EDE6FF",
+  },
+];
+// https://i.ibb.co/g97907v/1.png
+// https://i.ibb.co/k1fBR4Q/2.png
+// https://i.ibb.co/bswRv5N/3.png
+// https://i.ibb.co/VT8cyHk/4.png
+// https://i.ibb.co/PwVtg0G/5.png
