63b4eb411136e014e681f04f04aa34c75a896f82
Panha <76092165+Panha13@users.noreply.github.com>
2024-03-20 00:57:17 +0700

    categories end

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -7,12 +7,12 @@ const Category = ({ image, label, link }) => {
     <figure className="relative w-full h-64 rounded-md overflow-hidden cursor-pointer">
       <a href={link}>
         <img
-          className=" w-full h-full object-cover object-center"
+          className=" w-full h-full object-cover object-center transform transition-transform duration-500 hover:scale-105"
           src={image}
           alt={label}
         />
       </a>
-      <figcaption className="absolute inset-0 flex items-center justify-center bg-black/10">
+      <figcaption className="absolute inset-0 flex items-center justify-center pointer-events-none bg-black/10">
         <h1 className="text-4xl text-white uppercase font-medium">{label}</h1>
       </figcaption>
     </figure>
@@ -20,20 +20,21 @@ const Category = ({ image, label, link }) => {
 };
 
 const Categories = () => {
+  const categories = [
+    { image: img1, label: "Men", link: "#men" },
+    { image: img2, label: "Women", link: "#women" },
+    { image: img3, label: "Kids", link: "#kids" },
+  ];
   return (
     <section className="flex flex-row gap-3 m-5">
-      <figure className="relative w-full h-64 rounded-md overflow-hidden cursor-pointer">
-        <a href="#">
-          <img
-            className=" w-full h-full object-cover object-center"
-            src={img1}
-            alt=""
-          />
-        </a>
-        <figcaption className="absolute inset-0 flex items-center justify-center bg-black/10">
-          <h1 className="text-4xl text-white uppercase font-medium">Men</h1>
-        </figcaption>
-      </figure>
+      {categories.map((category, index) => (
+        <Category
+          key={index}
+          image={category.image}
+          label={category.label}
+          link={category.link}
+        />
+      ))}
     </section>
   );
 };
